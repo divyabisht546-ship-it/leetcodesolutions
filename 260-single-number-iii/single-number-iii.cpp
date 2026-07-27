f36dbfdfd31660d544cbc16e46b3bfbee4fcaea1
DIVYA BISHT <divyabisht546@gmail.com>
2026-07-27 10:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (28.88%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end()); // 1 1  2 2 3 5
-    int i=0;
-    int n=nums.size(); //6
-    vector<int>ans;
-   while(i<n-1){ //5
-    if(nums[i]!=nums[i+1]){
-ans.push_back(nums[i]);
-i++;
-    }
-    else{
-        i=i+2;
-    }
-   }
-   if(nums[n-1]!=nums[n-2]){
-  ans.push_back(nums[n-1]);
-   }
-   return ans;
+        int totalxor=0;
+        for(int n:nums){
+            totalxor^=n;
+        }
+        int first=0;
+        int second=0;
+        int bitsno=0;
+while(((totalxor>>bitsno)&1)!=1){
+    bitsno++;
+}
+        for(int i:nums){
+            if((i>>bitsno)&1==1){
+                first^=i;
+            }
+            else{
+                second^=i;
+            }
+        }
+return {first,second};
     }
 };
