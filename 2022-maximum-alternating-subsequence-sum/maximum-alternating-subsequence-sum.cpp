e76257c60f137cf84df49e30d9a0839054f08f39
DIVYA BISHT <divyabisht546@gmail.com>
2026-08-05 20:02:36 +0530

    Time: 278 ms (31.23%) | Memory: 157 MB (55.90%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 long long solve(int n,vector<vector<long long>>&dp,vector<int>&nums){
-    dp[0][0]=nums[0];
+    dp[0][0]=0;
     dp[0][1]=0;
 for(int i=1;i<=n;i++){
     dp[i][0]=max(dp[i-1][1]+nums[i-1],dp[i-1][0]);
