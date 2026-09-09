023fce5f7c4a1a42f2c8615369fe1b2952c3ea19
DIVYA BISHT <divyabisht546@gmail.com>
2026-09-09 11:42:10 +0530

    Time: 33 ms (22.77%) | Memory: 25.2 MB (53.98%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,67 +1,41 @@
 class Solution { 
 public: 
-// done by mee by mik bhaiya
-    int dx[8]={-1,-1,0,1,1,1,0,-1}; 
-    int dy[8]={0,-1,1,1,0,-1,-1,1}; 
-
-    bool isvalid(int x,int y, vector<vector<int>>&matrix,int n){ 
-        if(x<n && y<n && x>=0 && y>=0 && matrix[x][y]==0) 
-            return true; 
-        
-        return false; 
-    } 
-
+int dx[8]={-1,-1,0,1,1,1,0,-1};
+int dy[8]={0,-1,1,1,0,-1,-1,1};
+bool isvalid(int x,int y,vector<vector<int>>&matrix,int n){
+    if(x>=0 && y>=0 && x<n && y<n && matrix[x][y]==0) return true;
+    return false;
+}
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) { 
+        if(grid[0][0]==1) return -1;
+        typedef pair<int,pair<int,int>>p;
+         vector<vector<int>>matrix(grid.begin(),grid.end());
+         int n=matrix.size();
+//djikstra applied
+priority_queue<p,vector<p>,greater<p>>pq;
+vector<vector<int>>res(n,vector<int>(n,INT_MAX));
 
-        vector<vector<int>> matrix(grid.begin(),grid.end()); 
-
-        int n=matrix.size(); 
-
-        if(grid[0][0]==1 || grid[n-1][n-1]==1)
-            return -1;
-
-        queue<pair<int,int>> q; 
-
-        q.push({0,0}); 
-
-        // IMPORTANT: starting cell ko immediately visited mark karo
-        matrix[0][0]=1;
-
-        int count=1; 
-
-        while(!q.empty()){ 
-
-            int size=q.size(); 
-
-            while(size--){ 
-
-                int x=q.front().first; 
-                int y=q.front().second; 
-                q.pop(); 
-
-                // Destination reached
-                if(x==n-1 && y==n-1){ 
-                    return count; 
-                } 
-
-                for(int k=0;k<8;k++){ 
-
-                    int newx=dx[k]+x; 
-                    int newy=dy[k]+y; 
-
-                    if(isvalid(newx,newy,matrix,n)){ 
-
-                        // Queue mein daalte hi visited
-                        matrix[newx][newy]=1; 
-
-                        q.push({newx,newy}); 
-                    } 
-                } 
-            } 
-
-            count++; 
-        } 
-
-        return -1; 
+pq.push({1,{0,0}});
+ res[0][0]=1; //here was my mistake
+while(!pq.empty()){
+    int dist=pq.top().first;
+    int x=pq.top().second.first;
+    int y=pq.top().second.second;
+    pq.pop();
+    for(int k=0;k<8;k++){
+        int newx=dx[k]+x;
+        int newy=dy[k]+y;
+        if(isvalid(newx,newy,matrix,n)){
+            if(res[newx][newy]>dist+1){
+                res[newx][newy]=dist+1;
+                pq.push({res[newx][newy],{newx,newy}});
+            }
+        }
+    }
+}
+if(res[n-1][n-1]!=INT_MAX){
+    return res[n-1][n-1];
+}
+return -1;
     } 
 };
